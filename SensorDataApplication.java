75b9e9de1a57a45c6358915d00e924b813f7e5ed
YJ <anderskolby@hotmail.com>
2024-04-09 20:12:22 +0200

    Create SensorDataApplication.java

---
+++ SensorDataApplication.java
@@ -0,0 +1,12 @@
+package com.example.sensordata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SensorDataApplication.class, args);
+    }
+}
